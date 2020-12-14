4533a56b1cba2c13c9966bbc003280058a6d3de7
denishuang <szuprefix@126.com>
2020-12-14 15:38:01 +0800

    0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 setup(
     name="xyz-util",
-    version="0.0.8",
+    version="0.0.9",
     author="szuprefix",
     author_email="szuprefix@126.com",
     description="common utils",
